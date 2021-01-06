e6668b536cc357808a992d4b9aac30c03b997fe5
Jyoshna-12345 <58583682+Jyoshna-12345@users.noreply.github.com>
2021-01-06 14:32:56 +0530

    Update views.py

--- views.py
+++ views.py
@@ -1,18 +1,49 @@
 from django.shortcuts import render,redirect
-from lab_covid.models import Login
+from django.contrib.auth import authenticate,login
+from minip.forms import RegisterForm,IndexForm
 from django.http import HttpResponse
-from lab_covid.forms import Loginform
-# Create your views here.
-def login(request):
-	if request.method=="POST":
-	       username=request.POST['username']
-	       password=request.POST['password']
-	       data=Login.objects.create(username=username,password=password)
-	       return redirect('home')
-	return render(request,"lab_covid/login.html") 
+from minip.models import Index
+from django.contrib.auth.models import User
 
-def lab1(request):
-	return render(request,'lab_covid/lab1.html')
+# Create your views here.
+def signin(request):
+	if request.method=='POST':
+		username=request.POST['username']
+		password=request.POST['password']
+		user = authenticate(request,username=username,password=password)
+		print(user)
+		if user is not None:
+			login(request,user)
+			return redirect('/home')
+		else:
+			return HttpResponse("Success")
+	return render(request,'minip/signin.html')
+
+def register(request):
+	form=RegisterForm()
+	if request.method=='POST':
+		form = RegisterForm(request.POST)
+		if form.is_valid():
+			form.save()
+			user=form.cleaned_data.get('email')
+			print(user)
+			return redirect('/signin')
+	return render(request, 'minip/register.html', {'form': form})
 
 def home(request):
-	return render(request,'lab_covid/home.html')
+	return render(request,"minip/home.html")
+
+def index(request):
+	if request.method=="POST":
+		f_name=request.POST['f_name']
+		l_name=request.POST['l_name']
+		phonenumber=request.POST['phonenumber']
+		rollnumber=request.POST['rollnumber']
+		reservationdate=request.POST['reservationdate']
+		emailid=request.POST['emailid']
+		lab=request.POST['lab']
+		session=request.POST['session']
+		hint=Index(f_name=f_name,l_name=l_name,phonenumber=phonenumber,rollnumber=rollnumber,reservationdate=reservationdate,emailid=emailid,lab=lab,session=session)
+		hint.save()
+		return redirect('/home')
+	return render(request,"minip/index.html")
